1420166476d47bf4dad518499063b916e18ac4d4
Rui Cheng <ktei2008@gmail.com>
2018-08-18 17:56:51 +1000

    CORS

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const indexRouter = require("./routes/index");
@@ -18,6 +19,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(cors());
 
 // app.use("/", indexRouter);
 app.use("/api", jwtMiddleware);
