44149da1cb7fe7d8ca1c62d00ac4c1bd63a5ae04
Rui Cheng <ktei2008@gmail.com>
2018-08-19 17:30:16 +1000

    remove redundant code

--- src/app.js
+++ src/app.js
@@ -7,15 +7,9 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const itemsRouter = require("./routes/items");
-// const indexRouter = require("./routes/index");
-// const usersRouter = require("./routes/users");
 
 const jwtMiddleware = require("./middleware/jwt");
 const app = express();
-
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -25,10 +19,8 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(cors());
 
-// app.use("/", indexRouter);
 app.use("/", jwtMiddleware);
 app.use("/items", itemsRouter);
-// app.use("/users", usersRouter);
 
 app.use("/claims", (req, res) => {
   res.send(req.claims);
@@ -40,7 +32,7 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
